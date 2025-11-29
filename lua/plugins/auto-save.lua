4f600b1c8d555432132307d7eb513a18d6e31e2a
jon <johntezjohn502@gmail.com>
2025-11-29 23:13:32 +0300

    Java added

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,8 @@
+return {
+  "okuuva/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" },
+  opts = {
+    enabled = true,
+    -- execution_message section removed
+  },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,8 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    require("lspconfig").ts_ls.setup({
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    })
+  end,
+}
